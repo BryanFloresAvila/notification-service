1343d45d2d575a388f48d9fc197f69acec0688c9
Bryan Flores <elton.flores.a@uni.pe>
2023-01-06 12:44:07 -0500

    feat: initial server config

---
+++ src/network/server.ts
@@ -0,0 +1,43 @@
+import express from 'express'
+
+import { applyRoutes } from './router'
+
+class Server {
+  private app: express.Application
+
+  constructor() {
+    this.app = express()
+    this.config()
+  }
+
+  private config() {
+    this.app.set('port', (process.env.PORT as string) || 3000)
+    this.app.use(express.text())
+    this.app.use(
+      (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+      ) => {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header(
+          'Access-Control-Allow-Headers',
+          'Authorization, Content-Type'
+        )
+        next()
+      }
+    )
+    applyRoutes(this.app)
+  }
+
+  public start() {
+    this.app.listen(this.app.get('port'), () => {
+      console.log('Server is running on port', this.app.get('port'))
+    })
+  }
+}
+
+const server = new Server()
+
+export { server as Server }
